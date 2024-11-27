4c4cc2cfab7247087bc1c27959a26498a9a4368f
Sagadu0108 <165743001+Sagadu0108@users.noreply.github.com>
2024-11-28 00:18:57 +0700

    Delete settingbutton.js

--- settingbutton.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const stTable = document.querySelector('.st-table');
-    const btnPopup = document.querySelector('.button-setting');   
-    
-    btnPopup.addEventListener('click', () => {
-        stTable.classList.toggle('active-popup');
-    });
-    
-});
